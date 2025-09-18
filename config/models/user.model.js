e34ff5d958d63ebe4193ba48ddc85d2bf4ac873b
Jorge Daniel <jorgedanielriveroc@gmail.com>
2025-09-18 10:57:35 -0300

    first commit

---
+++ config/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    age: {
+        type: Number,
+        required: false
+    },
+    password: {
+        type: String,
+        required: function () { return !this.githubid; }
+    },
+    githubid: {
+        type: String,
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+    }
+}, { timestamps: true });
+
+export const User = mongoose.model('User', userSchema);
