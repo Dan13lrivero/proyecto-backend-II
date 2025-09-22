a0bc7345929a6c18b587bd14316def6094532762
Jorge Daniel <jorgedanielriveroc@gmail.com>
2025-09-22 11:31:36 -0300

    cambios post clase 04

--- middleware/auth.middlewar.js
+++ middleware/auth.middlewar.js
@@ -1,37 +1,10 @@
-import jwt from "jsonwebtoken";
+import passport from "passport";
 
-export function requireLogin(req, res, next) {
-    if (!req.session.user) {
-        return res.status(401).json({ error: "No Autorizado" });
-    }
+
+export const requireJwtCookie = passport.authenticate('jwt-cookie', {session: false});
+
+export const requireRole = (...role) => (req, rest, next) => {
+    if(!req.user) return rest.status(401).json({error: 'No autorizado'});
+    if(!roles.includes(req.user.role)) return rest.status(403).json({error: 'Acceso prohibido'});
     next();
-}
-
-export function alreadyLoggedIn(req, res, next) {
-    if (req.session.user) {
-        return res.status(403).json({ error: "Ya estas Logueado" });
-    }
-    next();
-}
-
-// Autorizacion por Roles
-export function requireRole(role) {
-    return (req, res, next) => {
-        const user = req.session?.user || req.user; // session o passport
-        if (!user) return res.status(401).json({ error: "No autorizado" });
-        if (user.role !== role) return res.status(403).json({ error: "Prohibido" });
-        next();
-    };
-}
-
-export function requiereJWT(req, res, next) {
-    const header = req.headers.authorization || "";
-    const token = header.startsWith("Bearer ") ? header.slice(7) : null;
-    if (!token) return res.status(401).json({ error: "Token faltante" });
-    try {
-        req.jwt = jwt.verify(token, process.env.JWT_SECRET);
-        next();
-    } catch {
-        return res.status(401).json({ error: "Token inválido/expirado" });
-    }
 }
